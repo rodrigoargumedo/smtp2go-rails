8cd45fd8c7d90b59ced89d23ecea6ca80c6752ea
Rodrigo Argumedo <7613139+rodrigoargumedo@users.noreply.github.com>
2024-12-20 19:15:24 +0000

    Update test dummy app to Rails 7.2

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,7 +18,6 @@ end
 # to be shown.
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
 
-Rails::TestUnitReporter.executable = 'bin/test'
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_path=)
